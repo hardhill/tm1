815422615425565f20903cc2785dca5e028c773d
hardhill <hardhill@mail.ru>
2021-04-14 20:16:04 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-row type="flex" align="center" justify="center">
+    <el-col :xs="24" :sm="20" :md="16" :lg="12" :xl="8">
+      <AppPost
+        v-for="post in 3"
+        :key="post"
+      />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  import AppPost from '@/components/main/Post'
+export default {
+    head:{
+      title:'Главный'
+    },
+    components:{
+      AppPost
+    }
+}
+</script>
+
+<style>
+
+</style>
